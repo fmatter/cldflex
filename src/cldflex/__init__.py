6c2ccba73c668873d10797fac684354d9fe9662e
Florian Matter <florianmatter@gmail.com>
2022-05-27 12:39:57 -0700

    0.0.2.dev

--- src/cldflex/__init__.py
+++ src/cldflex/__init__.py
@@ -6,4 +6,4 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Florian Matter"
 __email__ = "florianmatter@gmail.com"
-__version__ = "0.0.1"
+__version__ = "0.0.2.dev"
