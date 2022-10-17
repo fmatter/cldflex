9531a568beff19731a4d9ee8ed09da4bd97e271e
Florian Matter <florianmatter@gmail.com>
2022-10-17 14:31:16 -0700

    tests

--- tests/test_flex.py
+++ tests/test_flex.py
@@ -14,3 +14,13 @@ def test_convert(flextext, monkeypatch, tmp_path, data):
     with open(data / "output" / "sentences.csv", "r", encoding="utf-8") as f:
         sentences2 = f.read()
     assert sentences == sentences2
+
+def test_with_lexicon(flextext, monkeypatch, tmp_path, data):
+    convert(flextext, lexicon_file=data/"output"/"morphs.csv", output_dir=tmp_path)
+
+    with open(tmp_path / "form_slices.csv", "r", encoding="utf-8") as f:
+        sl1 = f.read()
+
+    with open(data / "output" / "form_slices.csv", "r", encoding="utf-8") as f:
+        sl2 = f.read()
+    assert sl1 == sl2
--- tests/test_lift.py
+++ tests/test_lift.py
@@ -5,6 +5,12 @@ import pytest
 from cldflex.lift2csv import convert
 
 
-def test_convert():
-    with pytest.raises(FileNotFoundError):
-        convert("nothing")
+def test_convert(data, tmp_path):
+    convert(lift_file=data/"ikpeng_lift.lift", output_dir=tmp_path)
+
+    with open(tmp_path / "morphs.csv", "r", encoding="utf-8") as f:
+        lexicon = f.read()
+
+    with open(data / "output" / "morphs.csv", "r", encoding="utf-8") as f:
+        lexicon2 = f.read()
+    assert lexicon == lexicon2
