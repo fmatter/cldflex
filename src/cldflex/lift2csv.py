e256fd59bd90921dce4176a8a7a6e59bb5918d01
Florian Matter <florianmatter@gmail.com>
2022-10-21 17:41:04 -0700

    extract dictionary examples

--- src/cldflex/lift2csv.py
+++ src/cldflex/lift2csv.py
@@ -6,7 +6,7 @@ import yaml
 from xmljson import badgerfish as bf
 from cldflex.helpers import listify
 from bs4 import BeautifulSoup
-
+from slugify import slugify
 
 log = logging.getLogger(__name__)
 
@@ -20,16 +20,16 @@ def get_variant(morphemes, main_id, forms):
     return out
 
 
-def gather_variants(entry, variants):
+def gather_variants(entry, variant_dict):
     relations = entry.find_all("relation")
     if relations:
         for relation in relations:
             if relation.select("trait[name='variant-type']") and relation["ref"] != "":
                 main_entry_id = relation["ref"].split("_")[-1]
-                variants.setdefault(main_entry_id, [])
-                variants[main_entry_id].append(
+                variant_dict.setdefault(main_entry_id, [])
+                variant_dict[main_entry_id].append(
                     {
-                        "ID": entry["id"],
+                        "ID": slugify(entry["id"]),
                         "Form": entry.find("form").text,
                         "Type": entry.select("trait[name='morph-type']")[0]["value"],
                     }
@@ -58,7 +58,7 @@ def get_morph_type(entry):
 # </entry>
 
 
-def parse_entry(entry, variant_dict=None, sep="; "):
+def parse_entry(entry, dictionary_examples, variant_dict=None, sep="; "):
     entry_id = entry["guid"]
     variant_dict = variant_dict or {}
     forms = []
@@ -66,14 +66,30 @@ def parse_entry(entry, variant_dict=None, sep="; "):
     poses = []
     morphs = []
     fields = {}
+
     for sense in entry.find_all("sense", recursive=False):
+        # POS
         for gramm in sense.find_all("grammatical-info"):
             poses.append(gramm["value"])
+            # glosses for this sense
         for gloss in sense.find_all("gloss"):
             key = "gloss_" + gloss["lang"]
             fields.setdefault(key, [])
             fields[key].append(gloss.text)
+            # examples are stored in senses
+        for ex_count, example in enumerate(sense.find_all("example")):
+            example_dict = {"ID": f"{entry_id}-{ex_count}"}
+            for child in example.find_all(recursive=False):
+                if child.name == "form":
+                    example_dict["Form"] = child.text
+                else:
+                    child_form = child.find("form")
+                    example_dict[f"{child.name}-{slugify(child['type'])}-{child_form['lang']}"] = child_form.text
+            for attr in example.attrs:
+                example_dict[attr] = example[attr]
+            dictionary_examples.append(example_dict)
 
+    # go through form(s?)
     form_count = 0
     for form in entry.find("lexical-unit").find_all("form"):
         f_dict = {
@@ -87,6 +103,7 @@ def parse_entry(entry, variant_dict=None, sep="; "):
         forms.append(form.text)
         form_count += 1
 
+    # go through allomorphs / variants
     for variant in entry.find_all("variant"):
         morph_type = get_morph_type(variant)
         for form in variant.find_all("form"):
@@ -100,6 +117,12 @@ def parse_entry(entry, variant_dict=None, sep="; "):
             morphs.append(f_dict)
             forms.append(form.text)
             form_count += 1
+
+    # gather variants stored in other dictionary entries
+    for variant in variant_dict.get(entry_id, []):
+        variant["Morpheme_ID"] = entry_id
+        variant.update(**fields)
+        morphs.append(variant)
 
     morpheme_dict = {
         "ID": entry_id,
@@ -131,6 +154,7 @@ def convert(
     morphs = []
     entries = []
     variant_dict = {}
+    dictionary_examples = []
     for entry in lexicon.find_all("entry"):
         if not gather_variants(entry, variant_dict):
             entries.append(entry)
@@ -141,7 +165,7 @@ def convert(
         if not obj_lg:
             obj_lg = entry.find("form")["lang"]
             log.info(f"Assuming [{obj_lg}] to be the object language")
-        morpheme, allomorphs = parse_entry(entry, sep=sep)
+        morpheme, allomorphs = parse_entry(entry, dictionary_examples,variant_dict=variant_dict, sep=sep)
         morphemes.append(morpheme)
         morphs.extend(allomorphs)
     morphemes = pd.DataFrame.from_dict(morphemes)
@@ -154,10 +178,11 @@ def convert(
             if isinstance(df[col].iloc[0], list):
                 df[col] = df[col].apply(lambda x: sep.join(x))
 
-    # print(morphs)
-    # print(morphemes)
     morphs.to_csv(output_dir / "morphs.csv", index=False)
     morphemes.to_csv(output_dir / "morphemes.csv", index=False)
+    if dictionary_examples:
+        dictionary_examples = pd.DataFrame.from_dict(dictionary_examples)
+        dictionary_examples.to_csv(output_dir / "dictionary_examples.csv", index=False)
 
 def convert1(lift_file="", id_map=None, gather_examples=True, output_dir=None):
     lift_file = Path(lift_file)
