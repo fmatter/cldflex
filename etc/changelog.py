8a7d0f5f2ac51de444f819c6c93c584819887192
Florian Matter <florianmatter@gmail.com>
2022-11-27 15:40:34 -0800

    remove etc

--- etc/changelog.py
+++
@@ -1,9 +0,0 @@
-import keepachangelog
-import configparser
-
-config = configparser.ConfigParser()
-config.read(".bumpversion.cfg")
-
-changes = keepachangelog.release(
-    "CHANGELOG.md", new_version=config["bumpversion"]["current_version"]
-)
--- etc/check_changelog.py
+++
@@ -1,5 +0,0 @@
-import keepachangelog
-import webbrowser
-
-changes = keepachangelog.to_dict("CHANGELOG.md", show_unreleased=True)
-webbrowser.open(changes["unreleased"]["url"], new=2)
